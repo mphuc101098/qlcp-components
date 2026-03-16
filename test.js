198568f5f6489a89c7510a2c4e5aad003cc54a98
Nguyen Minh Phuc <58518601+mphuc101098@users.noreply.github.com>
2026-03-16 15:31:12 +0700

    Refactor 'phuc-test' component structure and template
    Updated the Vue component 'phuc-test' to use a new template and added component options.

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
 var $moduleUI = {"data":[],"watch":{},"controls":[{"prop":"fluid grid-list-md","rows":[{"prop":"","cols":[{"el":"phuc-test","attr":{}}]}]}],"set":{}}
 
-Vue.component('phuc-test', {
-  template: `<h1>asdg agdkagdk gasd</h1>`});
+Vue.component('phuc-test',{template:` <div>phĂºc test comp</div> `, 
+		props: [],
+		data() {
+			return {}
+		},
+		mounted() {},
+		computed: {},
+		watch: {},
+		methods: {},
+	}
+);
