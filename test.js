6c81eb0fd32cbafcf6e567b7542b4f23e97a6027
Nguyen Minh Phuc <58518601+mphuc101098@users.noreply.github.com>
2026-03-16 15:29:49 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
+var $moduleUI = {"data":[],"watch":{},"controls":[{"prop":"fluid grid-list-md","rows":[{"prop":"","cols":[{"el":"phuc-test","attr":{}}]}]}],"set":{}}
+
 Vue.component('phuc-test', {
   template: `<h1>asdg agdkagdk gasd</h1>`});
