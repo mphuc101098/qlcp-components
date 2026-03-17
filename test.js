8a918924f92d55e6ec4db79c2fba806dd8bf0dc7
Leo Phạm <HongThaiPham@users.noreply.github.com>
2026-03-17 08:23:23 +0700

    ghf

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-var $moduleUI = {"data":[],"watch":{},"controls":[{"prop":"fluid grid-list-md","rows":[{"prop":"","cols":[{"el":"phuc-test","attr":{}}]}]}],"set":{}}
-
-Vue.component('phuc-test',{template:` <div>phĂºc test comp</div> `, 
-		props: [],
-		data() {
-			return {}
-		},
-		mounted() {},
-		computed: {},
-		watch: {},
-		methods: {},
-	}
-);
+Vue.component('phuc-test', {
+  template: `<h1>asdg agdkagdk gasd {{message}}</h1>`,
+  data(){
+    return {
+        message: 'leu lue uê'
+    }
+  }
+});
