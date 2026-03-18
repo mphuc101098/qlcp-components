4dc0286edd1ab80b2be8ee554f3958510590577f
Leo Phạm <HongThaiPham@users.noreply.github.com>
2026-03-18 14:36:01 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,2 @@
 console.log("xasdasbdakjsdbkasbdasbda");
 
-new Vue({
-  el: '#xxx-test-p',
-  template: `<h1>xxx-test-p ne</h1>`,
-})
-
-Vue.component('phuc-test', {
-  template: `<h1>asdg agdkagdk gasd {{message}}</h1>`,
-  data(){
-    return {
-        message: 'leu lue uê'
-    }
-  }
-});
