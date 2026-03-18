1418c9e5fc06f04a0236b719d9d058cab46b5016
Leo Phạm <HongThaiPham@users.noreply.github.com>
2026-03-18 14:50:56 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
-console.log("xasdasbdakjsdbkasbdasbda");
+console.log("from test");
 
+Vue.component('phuc-test', {
+        template: `
+    <div style="border:1px solid #ccc;padding:10px">
+        <h2>Phuc Test Component from test file</h2>
+        <div>{{ message }}</div>
+    </div>
+  `,
+        data: function () {
+            return {
+                message: "Hello from GitHub component"
+            }
+        }
+    });
