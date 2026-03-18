a47089763da84908bc0241095983c9efa903a4d2
Leo Phạm <HongThaiPham@users.noreply.github.com>
2026-03-18 15:32:23 +0700

    asdasda

--- all-comp.js
+++ all-comp.js
@@ -1,10 +1,13 @@
 app.component('te-le-le', {
-  template: `<div>Hello from te-le-le</div>`
+    props: ['data', 'ttt'],
+    template: `<div><div>Hello from te-le-le </div>
+        <div>{{ ttt }}</div>
+    </div>`
 });
 
 app.component('out-app', {
     template: `<div>{{ email }}</div>`,
-    data(){
+    data() {
         return {
             email: 'vueData.user.Email'
         }
